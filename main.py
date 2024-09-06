9a16056fa155cdcec09de63fe77a7ed9c86f5be2
nishan <nishanlama03@gmail.com>
2024-09-06 10:16:09 -0700

    converted data to CSV

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import numpy as np
 import os
-import matplotlib.pyplot as plt
-import seaborn as sns
 import json
+
 #from sklearn.model_selection import train_test_split
 #from sklearn.metrics import mean_sequared_error, r2_score
 
-file_path = 'queryData.json'
-df = pd.read_json(file_path, lines=True)
+google_data = os.path.join(os.getcwd(), 'Data', 'queryData.csv')
+df = pd.read_csv(google_data)
+print(df.head(10))
 
 print("hello")
